ebb294bdae2a36b9776d1e7e0f62691afb1a7ef6
hanbyeol.yang <beuri97@gmail.com>
2026-07-21 14:37:05 +0900

    init: initial commit

---
+++ src/main/java/io/regionevent/regioneventbackend/RegionEventBackendApplication.java
@@ -0,0 +1,13 @@
+package io.regionevent.regioneventbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionEventBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegionEventBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/regionevent/regioneventbackend/RegionEventBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.regionevent.regioneventbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionEventBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
