8b68c5b4cc1f25d22f709e6f7a8ecca332ca1ea2
hanbyeol.yang <beuri97@gmail.com>
2026-07-31 17:38:55 +0900

    test: 테스트 import 그룹을 정리한다

--- src/test/java/io/regionevent/regioneventbackend/domain/content/service/OriginalContentReviewTargetServiceTest.java
+++ src/test/java/io/regionevent/regioneventbackend/domain/content/service/OriginalContentReviewTargetServiceTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.Instant;
 
 import org.junit.jupiter.api.Test;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
