78f1d5f78c7743575fc5b73f046451888462f80a
kolyn092 <kolyn092@gmail.com>
2026-07-31 18:21:45 +0900

    feat: 정원 홀드 감사 대상 기반을 추가한다

--- src/test/java/io/regionevent/regioneventbackend/global/config/InitialP0SchemaMigrationTest.java
+++ src/test/java/io/regionevent/regioneventbackend/global/config/InitialP0SchemaMigrationTest.java
@@ -23,7 +23,7 @@ class InitialP0SchemaMigrationTest {
     }
 
     @Test
-    void 빈_데이터베이스에_V1부터_V10까지_현재_P0_스키마를_생성한다() {
+    void 빈_데이터베이스에_V1부터_V11까지_현재_P0_스키마를_생성한다() {
         List<String> appliedVersions = jdbcTemplate.queryForList(
             "SELECT \"version\" FROM \"flyway_schema_history\" WHERE \"version\" IS NOT NULL AND \"success\" = TRUE",
             String.class
@@ -73,7 +73,7 @@ class InitialP0SchemaMigrationTest {
             String.class
         );
 
-        assertThat(appliedVersions).containsExactly("1", "2", "3", "4", "5", "6", "7", "8", "9", "10");
+        assertThat(appliedVersions).containsExactly("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11");
         assertThat(tableNames).contains(
             "REGION",
             "APP_USER",
