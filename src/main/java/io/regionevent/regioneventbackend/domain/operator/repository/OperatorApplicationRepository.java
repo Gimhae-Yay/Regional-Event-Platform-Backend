91b1e9bad62432e96b7cca9025b165475332448a
minkim11 <alsgur4772@gmail.com>
2026-08-02 13:24:34 +0900

    feat: 운영자 권한 재신청 API를 추가한다

--- src/main/java/io/regionevent/regioneventbackend/domain/operator/repository/OperatorApplicationRepository.java
+++ src/main/java/io/regionevent/regioneventbackend/domain/operator/repository/OperatorApplicationRepository.java
@@ -3,6 +3,10 @@ package io.regionevent.regioneventbackend.domain.operator.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.regionevent.regioneventbackend.domain.operator.entity.OperatorApplication;
+import io.regionevent.regioneventbackend.domain.operator.entity.OperatorApplicationStatus;
+import io.regionevent.regioneventbackend.domain.user.entity.AppUser;
 
 public interface OperatorApplicationRepository extends JpaRepository<OperatorApplication, Long> {
+
+    boolean existsByApplicantAndStatus(AppUser applicant, OperatorApplicationStatus status);
 }
